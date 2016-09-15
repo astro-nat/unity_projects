5408a1c1ed0a12ac32f45491f121c63862fab22d
nano24432 <nano724@tamu.edu>
2016-09-15 13:20:42 -0500

    Added ALL files

---
+++ Roll-a-Ball/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float numObjects;
+	public GameObject origin;
+	public GameObject prefab;
+
+	private float spot = 0;
+
+	void Start() {
+		Vector3 center = origin.transform.position;
+		for (int i = 0; i < numObjects; i++){
+			Vector3 pos = RandomCircle(center, 5.0f, spot);
+			Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center-pos);
+			Instantiate(prefab, pos, rot);
+			spot = spot + 1/numObjects;
+		}
+	}
+
+	Vector3 RandomCircle ( Vector3 center ,   float radius , float spot ){
+		float ang = spot * 360;
+		Vector3 pos;
+		pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+		pos.y = center.y;
+		pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+		return pos;
+	}
+}
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public Vector3 origin;
+
+	void FixedUpdate() 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.position = origin;
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+
+}
